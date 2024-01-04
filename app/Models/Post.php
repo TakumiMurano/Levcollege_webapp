0592c60d0083bc59a3805f106a406c18880d7dd1
TakumiMurano <shiirosonya98@gmail.com>
2024-01-04 18:43:06 +0900

    カリキュラム08_2にてブログ投稿一覧のページネーションを追加しました

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,8 @@ class Post extends Model
     {
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
