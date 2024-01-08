27c04bef491aa77a5c3d8f696eae9a5d63fcb56e
TakumiMurano <shiirosonya98@gmail.com>
2024-01-08 17:21:14 +0900

    カリキュラム08_4、課題3にてブログ投稿を保存するプログラムを作成しました

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 });
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class , 'create']);
+Route::post('/posts', [PostController::class , 'store']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
